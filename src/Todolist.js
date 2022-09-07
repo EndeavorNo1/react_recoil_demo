a9f520f88de4e9521036997d6a4e815ee3b132f6
慧新 <31230553+EndeavorNo1@users.noreply.github.com>
2022-09-07 17:31:05 +0800

    feat: 删改查

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,6 +1,7 @@
 import { useRecoilValue } from "recoil";
 import TodoItemCreator from "./TodoItemCreator";
 import { todoListState } from "./recoil";
+import TodoItem from "./TodoItem";
 const TodoList = () => {
   const todoList = useRecoilValue(todoListState);
 
@@ -17,7 +18,4 @@ const TodoList = () => {
   );
 };
 
-const TodoItem = ({ key, item }) => {
-  return <div key={key}>{item.text}</div>;
-};
 export default TodoList;
